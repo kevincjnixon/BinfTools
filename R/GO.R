b05880c344acb75f391336f1023249bc3379518b
kevincjnixon <60985228+kevincjnixon@users.noreply.github.com>
2021-05-26 09:14:54 -0400

    Add files via upload

--- R/GO.R
+++ R/GO.R
@@ -15,13 +15,16 @@
 #' @param pdf Boolean indicating if bar plots should be exported to pdf. Default is TRUE.
 #' @param fig Boolean indicating if bar plots should be printed to R output. Default is TRUE.
 #' @param returnGost Boolean indicating if gost results should be returned for future use with gprofiler2 functions. Default is FALSE.
+#' @param writeRes Boolean indicating if GO.txt results should be written to file 'prefix.GO.txt'
+#' @param writeGem Boolean indicating if gem.txt results should be written to file.
 #' @return Exports a table of analysis results in 'prefix.GO.txt', a gem file in 'prefix.gem', and a pdf with two figures: top ten enriched terms followed by top ten significant terms in 'prefix.top10.pdf'
 #' @export
 #'
 
 GO_GEM<-function(geneList,species="hsapiens",bg=NULL,source=NULL, corr="fdr", iea=FALSE, prefix="GO_analysis", ts=c(10,500),
-                 pdf=T, fig=T, returnGost=F){
-  GOfun<-function(genes, spec=species, cbg=bg, dsource=source, corrm=corr, exiea=iea, prefix=pre, termsz=ts, prpdf=pdf, prfig=fig, giveGost=returnGost){
+                 pdf=T, fig=T, returnGost=F, writeRes=T, writeGem=T, returnRes=F){
+  GOfun<-function(genes, spec=species, cbg=bg, dsource=source, corrm=corr, exiea=iea, prefix=pre, termsz=ts, prpdf=pdf, prfig=fig, giveGost=returnGost,
+                  gemWrite=writeGem, resWrite=writeRes, giveRes=returnRes){
     #ts is term size (for plotting, terms must have ts genes to make cutoff, default is 10)
     x<-gprofiler2::gost(genes, organism=spec, custom_bg=cbg, sources=dsource, evcodes=TRUE, multi_query=FALSE, correction_method=corrm, exclude_iea=exiea)
     y<-x$result[,-14]
@@ -31,12 +34,20 @@ GO_GEM<-function(geneList,species="hsapiens",bg=NULL,source=NULL, corr="fdr", ie
     gem$FDR<-gem$p.Val
     gem$Phenotype="+1"
     gem<-gem[,c("GO.ID","Description","p.Val","FDR","Phenotype","Genes")]
-    write.table(gem, paste0(prefix,".gem.txt"), quote=FALSE, sep="\t", row.names = FALSE)
+    if(isTRUE(gemWrite)){
+      write.table(gem, paste0(prefix,".gem.txt"), quote=FALSE, sep="\t", row.names = FALSE)
+    }
     y<-y[order(y$enrichment, decreasing=T),]
     GO_plot(y, prefix, termsz, prpdf, prfig)
-    write.table(y, paste0(prefix,".GO.txt"), quote=FALSE, sep="\t", row.names=FALSE)
+    if(isTRUE(resWrite)){
+      write.table(y, paste0(prefix,".GO.txt"), quote=FALSE, sep="\t", row.names=FALSE)
+    }
     if(isTRUE(giveGost)){
       return(x)
+    } else {
+      if(isTRUE(giveRes)){
+        return(y)
+      }
     }
   }
   if(is.list(geneList)){
@@ -55,13 +66,28 @@ GO_GEM<-function(geneList,species="hsapiens",bg=NULL,source=NULL, corr="fdr", ie
       }
       return(gostList)
     } else {
-      for(i in 1:length(geneList)){
-        pre<-paste0(prefix,names(geneList)[i])
-        message("Analyzing ",names(geneList)[i]," and saving as: ",pre,"...")
-        tryCatch({GOfun(geneList[[i]], prefix=pre)},
-                 error=function(e){
-                   message("No significant results for ",names(geneList)[i],"...")
-                 })
+      if(isTRUE(returnRes)) {
+        resList<-list()
+        for(i in 1:length(geneList)){
+          pre<-paste0(prefix,names(geneList)[i])
+          message("Analyzing ",names(geneList)[i]," and saving as: ",pre,"...")
+          resList[[i]]<-tryCatch({GOfun(geneList[[i]], prefix=pre)},
+                                 error=function(e){
+                                   message("No significant results for ",names(geneList)[i],"...")
+                                   return(NA)
+                                 })
+          names(resList)[i]<-names(geneList)[i]
+        }
+        return(resList)
+      } else {
+        for(i in 1:length(geneList)){
+          pre<-paste0(prefix,names(geneList)[i])
+          message("Analyzing ",names(geneList)[i]," and saving as: ",pre,"...")
+          tryCatch({GOfun(geneList[[i]], prefix=pre)},
+                   error=function(e){
+                     message("No significant results for ",names(geneList)[i],"...")
+                   })
+        }
       }
     }
   } else {
@@ -71,7 +97,12 @@ GO_GEM<-function(geneList,species="hsapiens",bg=NULL,source=NULL, corr="fdr", ie
       x<-GOfun(geneList, prefix=pre)
       return(x)
     } else {
-      GOfun(geneList, prefix=pre)
+      if(isTRUE(returnRes)) {
+        x<- GOfun(geneList, prefix=pre)
+        return(x)
+      } else {
+        GOfun(geneList, prefix=pre)
+      }
     }
   }
 }
@@ -80,7 +111,7 @@ GO_GEM<-function(geneList,species="hsapiens",bg=NULL,source=NULL, corr="fdr", ie
 #'
 #' Function that generates the top 10 term figures for GO_GEM()
 
-GO_plot<-function(GOres, prefix, ts, pdf, fig){
+GO_plot<-function(GOres, prefix, ts, pdf, fig, print=c("both","sig","enr")){
   tmp<-GOres[order(GOres$p_value),] #order by increasing p-value
   #Take the top ten terms (already sorted by enrihcment)
   GOres<-GOres[which(GOres$term_size >= ts[1]),]
@@ -95,7 +126,7 @@ GO_plot<-function(GOres, prefix, ts, pdf, fig){
     ggplot2::scale_x_continuous(name="GO Term", breaks=1:10, labels=GOres$term_name) +
     ggplot2::scale_y_continuous(name="Enrichment", sec.axis=ggplot2::sec_axis(~(. -a)/b, name="-Log10 P-value")) +
     ggplot2::theme_classic() + ggplot2::theme(axis.title.y=ggplot2::element_text(color="blue"), axis.title.y.right=ggplot2::element_text(color="orange"),
-                            axis.text.x=ggplot2::element_text(angle=60, hjust=1)) +
+                                              axis.text.x=ggplot2::element_text(angle=60, hjust=1)) +
     ggplot2::labs(title=paste0("Top Ten Enriched terms (>",ts[1],"genes/term)"),
                   subtitle=subt)
   #Now get the top ten significant terms with less than 500 genes/term (in tmp)
@@ -106,18 +137,34 @@ GO_plot<-function(GOres, prefix, ts, pdf, fig){
     ggplot2::scale_x_continuous(name="GO Term", breaks=1:10, labels=tmp$term_name) +
     ggplot2::scale_y_continuous(name="Enrichment", sec.axis=ggplot2::sec_axis(~(. -a)/b, name="-Log10 P-value")) +
     ggplot2::theme_classic() + ggplot2::theme(axis.title.y=ggplot2::element_text(color="blue"), axis.title.y.right=ggplot2::element_text(color="orange"),
-                            axis.text.x=ggplot2::element_text(angle=60, hjust=1))+
+                                              axis.text.x=ggplot2::element_text(angle=60, hjust=1))+
     ggplot2::labs(title=paste0("Top Ten Significant terms (<",ts[2],"genes/term)"),
                   subtitle=subt)
   if(isTRUE(pdf)){
     pdf(paste0(prefix,".Top10.pdf"))
-    print(p)
-    print(q)
+    if(print[1]=="both"){
+      print(p)
+      print(q)
+    }
+    if(print[1]=="sig"){
+      print(q)
+    }
+    if(print[1]=="enr"){
+      print(p)
+    }
     dev.off()
   }
   if(isTRUE(fig)){
-    print(p)
-    print(q)
+    if(print[1]=="both"){
+      print(p)
+      print(q)
+    }
+    if(print[1]=="sig"){
+      print(q)
+    }
+    if(print[1]=="enr"){
+      print(p)
+    }
   }
 }
 
