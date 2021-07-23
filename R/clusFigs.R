6a95d57608e6e7ebcca953d594b56e5251f1f1f3
kevincjnixon <60985228+kevincjnixon@users.noreply.github.com>
2021-07-23 09:46:50 -0400

    Update clusFigs.R

--- R/clusFigs.R
+++ R/clusFigs.R
@@ -126,7 +126,7 @@ clusRelev<-function(clusRes, cluslev, rename=T, title="Releveled Clusters", col=
   clusRes<-as.data.frame(clusRes)
   clusRes$cluster<-as.numeric(clusRes$cluster)
   if(isTRUE(rename)){
-    newClus<-rep(NA, nrow(clusRes))
+    newClus<-c()
     for(i in 1:numClus){
       message("Cluster ",as.numeric(cluslev[i])," is now cluster ",i,".")
       newClus<-c(newClus, rep(i, length(clusRes$cluster[which(clusRes$cluster == as.numeric(cluslev[i]))])))
