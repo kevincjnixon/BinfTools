7074442b4c9e505a092a10625041b2feefe69ea1
kevincjnixon <60985228+kevincjnixon@users.noreply.github.com>
2022-06-02 10:25:07 -0400

    Update UpcomingFunctions.R

--- R/UpcomingFunctions.R
+++ R/UpcomingFunctions.R
@@ -1,3 +1,4 @@
+#Take a numeric vector (i.e. a vector of gene expression) and assign it into named quantiles of a desired length - great for making a 'condition' vector based off of gene expression
 quart_group<-function(x, groups=c("1st","2nd","3rd","4th")){
   q<-quantile(x, probs=seq(0,1, (1/length(groups))))
   g<-c(rep(groups[length(groups)], length(x)))
@@ -11,6 +12,8 @@ quart_group<-function(x, groups=c("1st","2nd","3rd","4th")){
   return(g)
 }
 
+#Grep a certain key from a GO results table's "term_name" column, and return provided columns
+#Great for making a termList for GOHeat (return "term_name" only) or getting intersection genes
 GOgrep<-function(GOtable, key, cols=c("term_name","intersection")){
   x<-GOtable[grep(key, GOtable$term_name, ignore.case=T),which(colnames(GOtable) %in% cols), drop=F]
   if(nrow(x)<1){
