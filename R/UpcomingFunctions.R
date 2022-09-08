bcc82d50964cc0187b52c00dd68bd7281309ddf4
kevincjnixon <60985228+kevincjnixon@users.noreply.github.com>
2022-09-08 10:43:51 -0400

    Update UpcomingFunctions.R

--- R/UpcomingFunctions.R
+++ R/UpcomingFunctions.R
@@ -25,3 +25,60 @@ GOgrep<-function(GOtable, key, cols=c("term_name","intersection")){
 na.rm<-function(x){
   return(x[!is.na(x)])
 }
+
+#Plot a correlation between two columns from two different or the same data frame or two named vectors
+#Great for comparing gene expression changes or values, or two gene rankings for GSEA
+plotCor<-function(x,y, xCol=F, yCol=F, xlab="", ylab="", title=""){
+  if(isFALSE(xCol)){
+    if(isFALSE(yCol)){
+      x<-x[which(names(x) %in% names(y))]
+      y<-y[which(names(y) %in% names(x))]
+    } else {
+      x<-x[which(names(x) %in% rownames(y))]
+      tmpNames<-rownames(y)[which(rownames(y) %in% names(x))]
+      y<-y[which(rownames(y) %in% names(x)),which(colnames(y) %in% yCol)]
+      names(y)<-tmpNames
+    }
+  } else {
+    if(isFALSE(yCol)){
+      tmpNames<-rownames(x)[which(rownames(x) %in% names(y))]
+      x<-x[which(rownames(x) %in% names(y)),which(colnames(x) %in% xCol)]
+      names(x)<-tmpNames
+      y<-y[which(names(y) %in% names(x))]
+    } else {
+      tmpNames<-rownames(x)[which(rownames(x) %in% rownames(y))]
+      x<-x[which(rownames(x) %in% rownames(y)), which(colnames(x) %in% xCol)]
+      names(x)<-tmpNames
+      tmpNames<-rownames(y)[which(rownames(y) %in% names(x))]
+      y<-y[which(rownames(y) %in% names(x)), which(colnames(y) %in% yCol)]
+      names(y)<-tmpNames
+    }
+  }
+  x<-x[order(names(x))]
+  y<-y[order(names(y))]
+  print(length(x))
+  print(length(y))
+  fit<-lm(y~x)
+  plot(x,y, main=title, pch=16, xlab=xlab, ylab=ylab)
+  abline(fit, lty=2, col="red")
+  legend("topleft", legend=paste0("R=",round(sqrt(summary(fit)$adj.r.squared),digits=4)))
+}
+
+#If you have n=1 for one sample, you can still make a 'pseudo MA plot'
+pseudoMA<-function(counts, treat, con, FoldChange=1, title="", retDA=F, retRes=F){
+  x<-data.frame(row.names=rownames(counts),
+                baseMean=rowMeans(counts[,which(colnames(counts) %in% c(treat, con)), drop=F]),
+                log2FoldChange=log2((1+rowMeans(counts[,which(colnames(counts) %in% treat),drop=F]))/(1+rowMeans(counts[,which(colnames(counts) %in% con),drop=F]))),
+                pvalue=rep(0.5,nrow(counts)),
+                padj=rep(0.5, nrow(counts)))
+  #Filter genes with baseMean==0
+  x<-subset(x, baseMean>0)
+  print(range(x$baseMean))
+  DA<-BinfTools::MA_Plot(x, title, 1, NULL, FC=FoldChange, returnDEG=T)
+  if(isTRUE(retDA)){
+    return(DA)
+  }
+  if(isTRUE(retRes)){
+    return(x)
+  }
+}
